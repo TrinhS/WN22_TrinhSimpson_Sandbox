c7bff36e21375706dbc55950c3ffa314651e4c14
TrinhS <trinh.simpson@lethbridgecollege.ca>
2022-03-15 00:00:04 -0600

    CharacterAnimationsExercise

---
+++ Assets/Scripts/AnimationTrigger.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationTrigger : MonoBehaviour
+{
+    public Animator animator;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P)) SwitchAnimation();
+    }
+
+    public void SwitchAnimation()
+    {
+        if (!animator.GetBool("Toggle"))
+        {
+            animator.SetBool("Toggle", true);
+        }
+        else animator.SetBool("Toggle", false);
+    }
+}
