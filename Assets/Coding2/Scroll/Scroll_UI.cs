7634c3412ebf21ac148b92f505243a91cba72d31
TrinhS <trinh.simpson@lethbridgecollege.ca>
2022-04-14 18:56:12 -0600

    Scroll UI prototype complete

--- Assets/Coding2/Scroll/Scroll_UI.cs
+++ Assets/Coding2/Scroll/Scroll_UI.cs
@@ -1,3 +1,4 @@
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -9,28 +10,53 @@ public class Scroll_UI : MonoBehaviour
     [SerializeField]
     GameObject open;
     [SerializeField]
-    GameObject scrollText;
+    GameObject scrollMenu;
     [SerializeField]
     GameObject closeButton;
     [SerializeField]
     GameObject openButton;
+    [SerializeField]
+    GameObject hideButton;
+    [SerializeField]
+    GameObject scrollButton;
+
     //new object is active, old object is disabled
     public void Close()
     {
         closed.SetActive(true);
         open.SetActive(false);
-        scrollText.SetActive(false);
+        scrollMenu.SetActive(false);
         closeButton.SetActive(false);
+        scrollButton.SetActive(false);
         openButton.SetActive(true);
+        hideButton.SetActive(true);
     }
 
     public void Open()
     {
         closed.SetActive(false);
         open.SetActive(true);
-        scrollText.SetActive(true);
+        scrollMenu.SetActive(true);
         closeButton.SetActive(true);
         openButton.SetActive(false);
+        scrollButton.SetActive(false);
+        hideButton.SetActive(false);
+    }
+
+    public void Hide()
+    {
+        closed.SetActive(false);
+        open.SetActive(false);
+        scrollMenu.SetActive(false);
+        closeButton.SetActive(false);
+        openButton.SetActive(false);
+        hideButton.SetActive(false);
+        scrollButton.SetActive(true);
+    }
+
+    public void Unhide()
+    {
+        Close();
     }
 }
 
